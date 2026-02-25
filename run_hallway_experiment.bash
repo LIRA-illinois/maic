c1563db31327cbad67fe01cf1257028840510c4d
Jacob <jheglun2@gmail.com>
2026-02-25 10:54:45 -0600

    updated experiment-runner fils

--- run_hallway_experiment.bash
+++ run_hallway_experiment.bash
@@ -1,4 +1,11 @@
 #!/bin/bash
-screen -dmS maic_hallway bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=0'
-screen -dmS maic_hallway bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=2289'
-screen -dmS maic_hallway bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=3608'
+# single runs
+screen -dmS maic_hallway_1 bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=0'
+screen -dmS maic_hallway_2 bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=2289'
+screen -dmS maic_hallway_3 bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=3608'
+
+# parallel runs
+screen -dmS maic_hallway_1_parallel bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=0 runner=parallel batch_size_run=20'
+screen -dmS maic_hallway_2_parallel bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=2289 runner=parallel batch_size_run=20'
+screen -dmS maic_hallway_3_parallel bash -c 'source .venv/bin/activate; python3 src/main.py --config=maic --env-config=join1 with seed=3608 runner=parallel batch_size_run=20'
+
